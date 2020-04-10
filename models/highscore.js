6aa80360fb4eec1ce6a7d95e971e45734c09d49f
Ana Valdivia <anav1317@yahoo.com>
2020-04-09 20:07:29 -0700

    added sequelized tables

--- models/highscore.js
+++ models/highscore.js
@@ -1,17 +1,14 @@
 module.exports = function(sequelize, DataTypes) {
-    var Highscore = sequelize.define("User", {
+    var Highscore = sequelize.define("Highscore", {
       // The email cannot be null, and must be a proper email before creation
-      email: {
+      user: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
-        }
       },
-      // The password cannot be null
-      password: {
-        type: DataTypes.STRING,
+      score: {
+        type: DataTypes.INT,
         allowNull: false
-      }
-    });
+    }
+})
+return Highscore;
+};
