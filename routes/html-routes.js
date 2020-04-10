94c7a439ef795172d183efefe99b5267fd82b3d0
Ana Valdivia <anav1317@yahoo.com>
2020-04-09 19:12:19 -0700

    Added files to edit

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,12 +6,20 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
+  // app.get("/", function(req, res) {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/members");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/signup.html"));
+  // });
+
   app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/game", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/game.html"));
   });
 
   app.get("/login", function(req, res) {
